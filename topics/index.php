6a469f38df06a31fc1cb43ccf30c9c719196884e
ichijimaosamu <ichijimaosamu@gmail.com>
2023-01-27 19:55:29 +0900

    DXトピックス一覧ページ追加

---
+++ topics/index.php
@@ -0,0 +1,129 @@
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/base_url.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/base_url.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>DXトピックス｜CMDS 神戸大学</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/common_head.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/common_head.php');
+}
+?>
+<link rel="stylesheet" href="<?php echo $base_url . '/'; ?>css/topics.css" />
+</head>
+<body id="topics" class="lower" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+
+<div id="container">
+<!--container-->
+
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/header.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/header.php');
+}
+?>
+
+<div id="content">
+<!--content-->
+
+<ol class="topicpath">
+<li class="topicpath__item"><a href="<?php echo $base_url . '/'; ?>">トップ</a></li>
+<li class="topicpath__item"><a href="<?php echo $base_url . $_SERVER["REQUEST_URI"]; ?>">DXトピックス</a></li>
+</ol>
+
+<main role="main">
+
+<article class="main">
+<!--main-->
+
+<h1 class="main__title"><span class="main__title__inner">DXトピックス</span></h1>
+
+<ul class="topics__list">
+<li class="topics__item"><a href="<?php echo $base_url . '/'; ?>">
+<p class="topics__item__thumb"><img src="<?php echo $base_url . '/'; ?>img/topics_item_thumb1.jpg" alt=""></p>
+<dl>
+<dt class="topics__item__term"><span class="topics__item__category">神戸データサイエンス操練所</span>2022.00.00</dt>
+<dd class="topics__item__description"><em>タイトルが入りますタイトルが入ります</em></dd>
+</dl>
+</a></li>
+<li class="topics__item"><a href="<?php echo $base_url . '/'; ?>">
+<p class="topics__item__thumb"><img src="<?php echo $base_url . '/'; ?>img/topics_item_thumb2.jpg" alt=""></p>
+<dl>
+<dt class="topics__item__term"><span class="topics__item__category">高大連携</span>2022.00.00</dt>
+<dd class="topics__item__description">タイトルが入りますタイトルが入ります</dd>
+</dl>
+</a></li>
+<li class="topics__item"><a href="<?php echo $base_url . '/'; ?>">
+<p class="topics__item__thumb"><img src="<?php echo $base_url . '/'; ?>img/topics_item_thumb3.jpg" alt=""></p>
+<dl>
+<dt class="topics__item__term"><span class="topics__item__category">DXプロジェクト</span>2022.00.00</dt>
+<dd class="topics__item__description">タイトルが入りますタイトルが入ります</dd>
+</dl>
+</a></li>
+<li class="topics__item"><a href="<?php echo $base_url . '/'; ?>">
+<p class="topics__item__thumb"><img src="<?php echo $base_url . '/'; ?>img/topics_item_thumb2.jpg" alt=""></p>
+<dl>
+<dt class="topics__item__term"><span class="topics__item__category">高大連携</span>2022.00.00</dt>
+<dd class="topics__item__description">タイトルが入りますタイトルが入ります</dd>
+</dl>
+</a></li>
+<li class="topics__item"><a href="<?php echo $base_url . '/'; ?>">
+<p class="topics__item__thumb"><img src="<?php echo $base_url . '/'; ?>img/topics_item_thumb3.jpg" alt=""></p>
+<dl>
+<dt class="topics__item__term"><span class="topics__item__category">DXプロジェクト</span>2022.00.00</dt>
+<dd class="topics__item__description">タイトルが入りますタイトルが入ります</dd>
+</dl>
+</a></li>
+</ul>
+
+
+<!--/main-->
+</article>
+
+</main>
+
+<!--/content-->
+</div>
+
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/footer.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/footer.php');
+}
+?>
+
+<!--/container-->
+</div>
+
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/common_script.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/common_script.php');
+}
+?>
+
+<!--sp_menu-->
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/sp_menu.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/sp_menu.php');
+}
+?>
+<!--/sp_menu-->
+
+</body>
+</html>
